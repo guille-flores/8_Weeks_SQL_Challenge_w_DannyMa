510f071d948d7c70e34da582e7338875e4ef0e2c
guille-flores <96453138+guille-flores@users.noreply.github.com>
2023-01-26 15:38:47 -0600

    Update 02_SQL_Queries.sql

--- CaseStudy_1/02_SQL_Queries.sql
+++ CaseStudy_1/02_SQL_Queries.sql
@@ -8,10 +8,33 @@ INNER JOIN dannys_diner.menu ON sales.product_id = menu.product_id
 GROUP BY sales.customer_id
 ORDER BY sales.customer_id ASC
 
-
-
+/*
+| customer | total_spent |
+| -------- | ----------- |
+| A        | 76          |
+| B        | 74          |
+| C        | 36          |
+*/
 
 -- 2. How many days has each customer visited the restaurant?
+SELECT 
+  sales.customer_id AS Customer,
+  COUNT(DISTINCT sales.order_date) AS Visits /* Multiple orders on same days shouldn't count, that's why we are using distincts dates*/
+FROM dannys_diner.sales sales
+GROUP BY sales.customer_id
+ORDER BY sales.customer_id
+
+
+/* 
+
+| customer | visits |
+| -------- | ------ |
+| A        | 4      |
+| B        | 6      |
+| C        | 2      |
+
+*/
+
 -- 3. What was the first item from the menu purchased by each customer?
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 -- 5. Which item was the most popular for each customer?
