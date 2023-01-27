68d7eb12aafc2acf28c443013a779bf9938ab30c
guille-flores <96453138+guille-flores@users.noreply.github.com>
2023-01-26 19:27:40 -0600

    Update 02_SQL_Queries.sql

--- CaseStudy_1/02_SQL_Queries.sql
+++ CaseStudy_1/02_SQL_Queries.sql
@@ -36,6 +36,35 @@ ORDER BY sales.customer_id
 */
 
 -- 3. What was the first item from the menu purchased by each customer?
+
+SELECT
+	temp.Customer_ID,  
+    temp.order_dates,
+    temp.products
+FROM (
+  SELECT 
+      sales.customer_id AS Customer_ID, 
+      sales.order_date AS order_dates,
+      STRING_AGG(menu.product_name, ', ') AS products, /*Concatenate the producst for each day and customer*/
+  	  ROW_NUMBER() OVER(PARTITION BY sales.customer_id ORDER BY sales.order_date) AS row_num /*Get the row number by sorting it by date and partition over customer*/
+  FROM dannys_diner.sales sales
+  INNER JOIN dannys_diner.menu menu ON sales.product_id = menu.product_id
+  GROUP BY sales.order_date, sales.customer_id /*Group by customer and date*/
+) temp
+WHERE row_num = 1 /*only get the 1st row=first order*/
+
+/*
+
+| customer_id | order_dates              | products     |
+| ----------- | ------------------------ | ------------ |
+| A           | 2021-01-01T00:00:00.000Z | sushi, curry |
+| B           | 2021-01-01T00:00:00.000Z | curry        |
+| C           | 2021-01-01T00:00:00.000Z | ramen, ramen |
+
+*/
+
+
+
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 -- 5. Which item was the most popular for each customer?
 -- 6. Which item was purchased first by the customer after they became a member?
